ddd119ad67509034b7b36c5c0327baeb081e7746
unknown <rocklion224@gmail.com>
2022-12-27 17:44:22 -0500

    Question 125

---
+++ 125_valid_palindrome.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cctype>
+#include<sstream>
+#include<algorithm>
+using namespace std;
+
+bool isPalindrome(string s) {
+    stringstream str(s);
+    char word;
+    string S;
+    while(str>>word){
+        if(iswalnum(word)){
+             S += word;
+        }
+    }
+    transform(S.begin(), S.end(), S.begin(), ::tolower);
+//    cout << S << endl;
+    int n=S.length();
+    for (int i = 0; i < n / 2; i++) {
+ 
+        if (S[i] != S[n - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+int main(){
+    cout<<isPalindrome("A man, a plan, a canal: Panama");
+}
