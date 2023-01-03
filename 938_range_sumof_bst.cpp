1758330f302a15e808347eea45fb4c563575e9ab
unknown <rocklion224@gmail.com>
2023-01-03 18:57:18 -0500

    938

---
+++ 938_range_sumof_bst.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+     TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root==nullptr){
+            return 0;
+        }
+        else if(root->val >= low && root->val <=high){
+            return root->val + rangeSumBST(root->left,low,high) + rangeSumBST(root->right,low,high);
+        }
+        return rangeSumBST(root->left,low,high) + rangeSumBST(root->right,low,high);
+    }
+}
+
