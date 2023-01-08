c2d3ad26729a9f8475a5317aa528a5628fab8a16
unknown <rocklion224@gmail.com>
2023-01-08 00:33:59 -0500

    q66

---
+++ 66_plus_one.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+ vector<int> plusOne(vector<int>& d) {
+        for(int i=d.size()-1;i>=0;i--)  {
+          if(d[i]<9){
+              d[i]++;
+              return d;
+          }
+          else{
+              d[i]=0;
+          }
+      }
+        d.push_back(0);
+        d[0]=1;
+        return d;
+    }
+
+int main(){
+
+}
