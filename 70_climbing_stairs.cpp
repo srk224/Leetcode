51985146ed36252ca31f54f84565d5122c40bbf6
unknown <rocklion224@gmail.com>
2023-01-10 01:31:03 -0500

    q70

---
+++ 70_climbing_stairs.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int climbStairs(int n) {
+      if(n%2==0){
+          return n/2 + 1;
+      }
+      else {
+          return n;
+      }
+    }
+
+int main(){
+
+}
