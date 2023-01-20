36e03e5dda5bf6afa9819a04194286fceb90ea04
unknown <rocklion224@gmail.com>
2023-01-19 19:10:32 -0500

    Binary Search

---
+++ Binary_Search.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool binarySearch(vector<int> arr,int n,int k){
+    int st = 0;
+    int end=arr.size()-1;
+    while(st<=end){
+         int mid = (st + end) / 2;
+        if(arr[mid]==k){
+            return true;
+        }
+        if(arr[mid]>k){
+            end = mid - 1;
+        }
+        else{
+            st = mid + 1;
+        }
+    }
+    return false;
+}
+
+int main(){
+    vector<int> arr = {1, 2, 3, 4, 5, 6};
+    cout << binarySearch(arr, 6, 0);
+}
