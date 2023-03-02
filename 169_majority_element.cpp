8eca7d1130d06e1ec0059fda859a4fac4736f418
ABDUL SHAHRUKH KHAN <rocklion224@gmail.com>
2023-03-02 16:26:54 -0500

    Create 169_majority_element.cpp

---
+++ 169_majority_element.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int majorityElement(vector<int>& nums) {
+        map<int,int> mp;
+        int ans=0;
+        for(int i=0;i<nums.size();i++)
+        mp[nums[i]]++;
+
+        for(auto i: mp){
+            if(i.second>nums.size()/2)
+            ans=i.first;
+        }
+        return ans;
+    }
