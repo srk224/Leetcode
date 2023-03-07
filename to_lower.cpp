7b29e0f6c5a0b2066bff47cceed2f1b47360ad51
ABDUL SHAHRUKH KHAN <rocklion224@gmail.com>
2023-03-07 15:01:19 -0500

    Create to_lower.cpp

---
+++ to_lower.cpp
@@ -0,0 +1,8 @@
+string toLowerCase(string s) {
+        for(int i=0;i<s.length();i++){
+            if(isupper(s[i])){
+                s[i]=tolower(s[i]);
+            }
+        }
+        return s;
+    }
