c6b4312cef41296cd079375e573de5da0010383f
unknown <rocklion224@gmail.com>
2023-03-10 20:13:50 -0500

    Question 682

---
+++ 713_subarray_product_less_than_k.cpp
@@ -0,0 +1,23 @@
+#inlude<iostream>
+#include<vector>
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k<= 1)return 0;
+        
+        int i=0,j=0;
+        int temp= 1,ans=0;
+        
+        while(j< nums.size()){
+            temp*= nums[j];
+            
+            while(i<= j && !(temp<k)){
+                temp/= nums[i];
+                i++;
+            }
+            
+            ans+= (j-i+1);
+            j++;
+        }
+        
+        return ans;
+    }
+
