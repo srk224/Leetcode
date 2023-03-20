8dec1a3e9d16d54d5ebe4a4b221fef06c5fbf6c1
ABDUL SHAHRUKH KHAN <rocklion224@gmail.com>
2023-03-20 13:00:12 -0400

    Create 62_unique_path.cpp

---
+++ 62_unique_path.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int uniquePaths(int m, int n) {
+        long ans = 1;
+        for(int i = m+n-2, j = 1; i >= max(m, n); i--, j++) 
+            ans = (ans * i) / j;
+        return ans;
+    }
