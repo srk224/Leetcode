739d16eac94223aaa4b8db28b6544c06a9e3d259
unknown <rocklion224@gmail.com>
2023-04-18 23:39:43 -0400

    Changes

---
+++ 78_subsets.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> ans;
+    vector<int> empty_arr = {};
+    ans.push_back(empty_arr);
+    for (int i = 0; i < nums.size();i++){
+        vector<int> push_arr;
+        push_arr.push_back(nums[i]); 
+        ans.push_back(push_arr);
+    }
+    for(auto i:nums){
+        for(auto j:nums){
+            cout << j << endl;
+        }
+    }
+}
+
+int main(){
+    vector<int> arr={1,2,3,4};
+    subsets(arr);
+}
